8f867b061b11072d893296be0187fb2180bfca75
Eugene <spring_86@mail.ru>
2024-10-20 16:56:41 +0300

    Additional check of frame loading

--- src/ui/Cursor.cpp
+++ src/ui/Cursor.cpp
@@ -2,6 +2,9 @@
 #include "animation/AnimationManager.hpp"
 #include "ui/Cursor.hpp"
 
+#define FRAME_RELEASED "Released"
+#define FRAME_CAPTURED "Captured"
+
 Cursor::Cursor() noexcept:
     m_isCaptured(false)
 {
@@ -14,7 +17,11 @@ bool Cursor::load(AnimationManager& animator) noexcept
 {
     m_frames = animator.loadFramesFromFile(CURSOR_FRAME_XML);
 
-    return !m_frames.empty();
+    for(auto frame: {FRAME_RELEASED, FRAME_CAPTURED})
+        if(auto found = m_frames.find(frame); found == m_frames.end())
+            return false;
+
+    return true;
 }
 
 void Cursor::capture() noexcept
