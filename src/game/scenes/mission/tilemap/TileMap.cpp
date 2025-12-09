b08126982a0f48ec9ae7f122b657014dfb3f0e97
Eugene <spring_86@mail.ru>
2025-12-09 09:18:01 +0300

    TileMap: added tiled collision mask

--- src/game/scenes/mission/tilemap/TileMap.cpp
+++ src/game/scenes/mission/tilemap/TileMap.cpp
@@ -18,6 +18,8 @@ struct Tileset
 };
 
 
+static char convert_tile_num_to_char(int index) noexcept;
+
 TileMap::TileMap() noexcept:
 	m_mapSize(glms_ivec2_zero()),
 	m_tileSize(glms_ivec2_zero())
@@ -29,12 +31,7 @@ TileMap::TileMap() noexcept:
 bool TileMap::loadFromFile(const std::filesystem::path &filePath) noexcept
 {
 //  Make sure it hasn't been downloaded before
-	m_title.clear();
-	m_vertices.clear();
-	m_indices.clear();
-	m_objects.clear();
-	m_mapSize = glms_ivec2_zero();
-	m_tileSize = glms_ivec2_zero();
+	reset();
 
 	if (filePath.empty())
 		return false;
@@ -63,11 +60,12 @@ bool TileMap::loadFromFile(const std::filesystem::path &filePath) noexcept
 		if( ! (mapWidth && mapHeight && tileWidth && tileHeight) )
 			return false;
 
-		m_mapSize  = { mapWidth,  mapHeight  };
-		m_tileSize = { tileWidth, tileHeight };
-
 		m_vertices.reserve(mapWidth * mapHeight * 4);
 		m_indices.reserve(mapWidth * mapHeight * 6);
+		m_tileMask.resize(mapWidth * mapHeight);
+
+		m_mapSize  = { mapWidth,  mapHeight  };
+		m_tileSize = { tileWidth, tileHeight };
 
 		if(loadLayers(static_cast<const void*>(mapNode)))
 			if(loadObjects(static_cast<const void*>(mapNode)))
@@ -81,6 +79,18 @@ bool TileMap::loadFromFile(const std::filesystem::path &filePath) noexcept
 bool TileMap::loadFromSQLiteDb(const std::filesystem::path& filePath) noexcept
 {
 	return false;
+}
+
+
+void TileMap::reset() noexcept
+{
+	m_title.clear();
+	m_vertices.clear();
+	m_indices.clear();
+	m_objects.clear();
+	m_tileMask.clear();
+	m_mapSize = glms_ivec2_zero();
+	m_tileSize = glms_ivec2_zero();
 }
 
 
@@ -105,6 +115,12 @@ std::span<const uint32_t> TileMap::getIndices() const noexcept
 std::span<const TileMap::Object> TileMap::getObjects() const noexcept
 {
 	return m_objects;
+}
+
+
+std::string_view TileMap::getTileMask() const noexcept
+{
+	return m_tileMask;
 }
 
 
@@ -251,10 +267,12 @@ void TileMap::loadLandscape(const Tileset& tileset, std::span<const int> tileIds
 		ratio.y = 1.f / textureHeight;
 	}
 
-	int mapWidth   = m_mapSize.x;
-	int mapHeight  = m_mapSize.y;
-	int tileWidth  = m_tileSize.x;
-	int tileHeight = m_tileSize.x;
+	const int mapWidth   = m_mapSize.x;
+	const int mapHeight  = m_mapSize.y;
+	const int tileWidth  = m_tileSize.x;
+	const int tileHeight = m_tileSize.x;
+
+	size_t tileIndex = 0;
 
 	for (int y = 0; y < mapHeight; ++y)
 	{
@@ -262,6 +280,9 @@ void TileMap::loadLandscape(const Tileset& tileset, std::span<const int> tileIds
 		{
 			const int tileID = tileIds[y * mapWidth + x];
 			const int tileNum = tileID - tileset.firstGID;
+
+			m_tileMask[tileIndex] = convert_tile_num_to_char(tileID);
+			++tileIndex;
 
 			const int tileY = (tileNum >= tileset.columns) ? tileNum / tileset.columns : 0;
 			const int tileX = tileNum % tileset.columns;
@@ -408,4 +429,283 @@ void TileMap::loadStructures(const Tileset& tileset, std::span<const int> tileId
 			}
 		}
 	}
+}
+
+
+char convert_tile_num_to_char(int index) noexcept
+{
+#if defined(__GNUC__) || defined(__MINGW32__)
+
+	switch (index)
+	{
+//      rocky soil
+		case 1 ... 5:     return 'R';
+		case 13 ... 16:   return 'R';
+		case 25 ... 27:   return 'R';
+		case 31 ... 34:   return 'R';
+		case 40 ... 42:   return 'R';
+		case 45:          return 'R';
+		case 48:          return 'R';
+		case 55:          return 'R';
+		case 61:          return 'R'; 
+		case 82:          return 'R';
+		case 83:          return 'R';
+		case 101:         return 'R';
+//      sandy soil
+		case 6 ... 12:    return 'S';
+		case 17 ... 24:   return 'S';	
+		case 28 ... 30:   return 'S';
+		case 35 ... 39:   return 'S';
+		case 43:          return 'S';
+		case 44:          return 'S';
+		case 46:          return 'S';
+		case 47:          return 'S';
+		case 49 ... 54:   return 'S';
+		case 56 ... 60:   return 'S';
+		case 62 ... 81:   return 'S';
+		case 84 ... 100:  return 'S';
+		case 102 ... 109: return 'S';
+//      NOTE: Tiles occupied by buildings will be filled in later when loaded by the Builder class
+//      Wall
+		case 111 ... 122: return 'R';
+//      Building
+		case 124 ... 137: return 'R';
+		case 140 ... 153: return 'R';
+		case 159 ... 165: return 'R';
+		case 175 ... 181: return 'R';
+		case 192 ... 195: return 'R';
+		case 207 ... 209: return 'R';
+		case 223 ... 225: return 'R';
+		case 239 ... 241: return 'R';	
+		case 255 ... 286: return 'R';
+//      Concrete slab
+		case 191:         return 'R';
+//      sandy soil by default
+		default:          return 'S';
+	}
+
+#elif defined(_MSC_VER)
+
+	switch (index)
+	{
+//      rocky soil
+		case 1:   return 'R';
+		case 2:   return 'R';
+		case 3:   return 'R';
+		case 4:   return 'R';
+		case 5:   return 'R';
+		case 13:  return 'R';
+		case 14:  return 'R';
+		case 15:  return 'R';
+		case 16:  return 'R';
+		case 25:  return 'R';
+		case 26:  return 'R';
+		case 27:  return 'R';
+		case 31:  return 'R';
+		case 32:  return 'R';
+		case 33:  return 'R';
+		case 34:  return 'R';
+		case 40:  return 'R';
+		case 41:  return 'R';
+		case 42:  return 'R';
+		case 45:  return 'R';
+		case 48:  return 'R';
+		case 55:  return 'R';
+		case 61:  return 'R'; 
+		case 82:  return 'R';
+		case 83:  return 'R';
+		case 101: return 'R';
+//      sandy soil
+		case 6:   return 'S';
+		case 7:   return 'S';
+		case 8:   return 'S';
+		case 9:   return 'S';
+		case 10:  return 'S';
+		case 11:  return 'S';
+		case 12:  return 'S';
+		case 17:  return 'S';
+		case 18:  return 'S';
+		case 19:  return 'S';
+		case 20:  return 'S';
+		case 21:  return 'S';
+		case 22:  return 'S';
+		case 23:  return 'S';
+		case 24:  return 'S';
+		case 28:  return 'S';
+		case 29:  return 'S';
+		case 30:  return 'S';
+		case 35:  return 'S';
+		case 36:  return 'S';
+		case 37:  return 'S';
+		case 38:  return 'S';
+		case 39:  return 'S';
+		case 43:  return 'S';
+		case 44:  return 'S';
+		case 46:  return 'S';
+		case 47:  return 'S';
+		case 49:  return 'S';
+		case 50:  return 'S';
+		case 51:  return 'S';
+		case 52:  return 'S';
+		case 53:  return 'S';
+		case 54:  return 'S';
+		case 56:  return 'S';
+		case 57:  return 'S';
+		case 58:  return 'S';
+		case 59:  return 'S';
+		case 60:  return 'S';
+		case 62:  return 'S';
+		case 63:  return 'S';
+		case 64:  return 'S';
+		case 65:  return 'S';
+		case 66:  return 'S';
+		case 67:  return 'S';
+		case 68:  return 'S';
+		case 69:  return 'S';
+		case 70:  return 'S';
+		case 71:  return 'S';
+		case 72:  return 'S';
+		case 73:  return 'S';
+		case 74:  return 'S';
+		case 75:  return 'S';
+		case 76:  return 'S';
+		case 77:  return 'S';
+		case 78:  return 'S';
+		case 79:  return 'S';
+		case 80:  return 'S';
+		case 81:  return 'S';
+		case 84:  return 'S';
+		case 85:  return 'S';
+		case 86:  return 'S';
+		case 87:  return 'S';
+		case 88:  return 'S';
+		case 89:  return 'S';
+		case 90:  return 'S';
+		case 91:  return 'S';
+		case 92:  return 'S';
+		case 93:  return 'S';
+		case 94:  return 'S';
+		case 95:  return 'S';
+		case 96:  return 'S';
+		case 97:  return 'S';
+		case 98:  return 'S';
+		case 99:  return 'S';
+		case 100: return 'S';
+		case 102: return 'S';
+		case 103: return 'S';
+		case 104: return 'S';
+		case 105: return 'S';
+		case 106: return 'S';
+		case 107: return 'S';
+		case 108: return 'S';
+		case 109: return 'S';
+//      Wall
+		case 111: return 'R';
+		case 112: return 'R';
+		case 113: return 'R';
+		case 114: return 'R';
+		case 115: return 'R';
+		case 116: return 'R';
+		case 117: return 'R';
+		case 118: return 'R';
+		case 119: return 'R';
+		case 120: return 'R';
+		case 121: return 'R';
+		case 122: return 'R';
+		case 124: return 'R';
+		case 125: return 'R';
+		case 126: return 'R';
+		case 127: return 'R';
+		case 128: return 'R';
+		case 129: return 'R';
+		case 130: return 'R';
+		case 131: return 'R';
+		case 132: return 'R';
+		case 133: return 'R';
+		case 134: return 'R';
+		case 135: return 'R';
+		case 136: return 'R';
+		case 137: return 'R';
+		case 140: return 'R';
+		case 141: return 'R';
+		case 142: return 'R';
+		case 143: return 'R';
+		case 144: return 'R';
+		case 145: return 'R';
+		case 146: return 'R';
+		case 147: return 'R';
+		case 148: return 'R';
+		case 149: return 'R';
+		case 150: return 'R';
+		case 151: return 'R';
+		case 152: return 'R';
+		case 153: return 'R';
+		case 159: return 'R';
+		case 160: return 'R';
+		case 161: return 'R';
+		case 162: return 'R';
+		case 163: return 'R';
+		case 164: return 'R';
+		case 165: return 'R';
+		case 175: return 'R';
+		case 176: return 'R';
+		case 177: return 'R';
+		case 178: return 'R';
+		case 179: return 'R';
+		case 180: return 'R';
+		case 181: return 'R';
+		case 192: return 'R';
+		case 193: return 'R';
+		case 194: return 'R';
+		case 195: return 'R';
+		case 207: return 'R';
+		case 208: return 'R';
+		case 209: return 'R';
+		case 223: return 'R';
+		case 224: return 'R';
+		case 225: return 'R';
+		case 239: return 'R';
+		case 240: return 'R';
+		case 241: return 'R';
+		case 255: return 'R';
+		case 256: return 'R';
+		case 257: return 'R';
+		case 258: return 'R';
+		case 259: return 'R';
+		case 260: return 'R';
+		case 261: return 'R';
+		case 262: return 'R';
+		case 263: return 'R';
+		case 264: return 'R';
+		case 265: return 'R';
+		case 266: return 'R';
+		case 267: return 'R';
+		case 268: return 'R';
+		case 269: return 'R';
+		case 270: return 'R';
+		case 271: return 'R';
+		case 272: return 'R';
+		case 273: return 'R';
+		case 274: return 'R';
+		case 275: return 'R';
+		case 276: return 'R';
+		case 277: return 'R';
+		case 278: return 'R';
+		case 279: return 'R';
+		case 280: return 'R';
+		case 281: return 'R';
+		case 282: return 'R';
+		case 283: return 'R';
+		case 284: return 'R';
+		case 285: return 'R';
+		case 286: return 'R';
+//      Concrete slab
+		case 191: return 'R';
+//      sandy soil by default
+		default:  return 'S';
+	}
+
+#else
+    #error "Unsupported compiler"
+#endif
 }
--- src/game/scenes/mission/tilemap/TileMap.hpp
+++ src/game/scenes/mission/tilemap/TileMap.hpp
@@ -32,12 +32,14 @@ public:
 
 	bool loadFromFile(const std::filesystem::path& filePath) noexcept;
 	bool loadFromSQLiteDb(const std::filesystem::path& filePath) noexcept;
+	void reset() noexcept;
 
 	std::string_view getTitle() const noexcept;
 
 	std::span<const vec4s>    getVertices() const noexcept;
 	std::span<const uint32_t> getIndices()  const noexcept;
 	std::span<const Object>   getObjects()  const noexcept;
+	std::string_view          getTileMask() const noexcept;
 
 	ivec2s getMapSize()  const noexcept;
 	ivec2s getTileSize() const noexcept;
@@ -53,6 +55,7 @@ private:
 	std::vector<vec4s>    m_vertices;
 	std::vector<uint32_t> m_indices;
 	std::vector<Object>   m_objects;
+	std::string           m_tileMask;
 	
 	ivec2s m_mapSize;
 	ivec2s m_tileSize;
